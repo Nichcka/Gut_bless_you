fa4102b409e3a131428425c3cdef00ddf5e4b496
Nichcka <115113064+Nichcka@users.noreply.github.com>
2025-08-09 19:19:56 +0300

    Add files via upload

---
+++ run_salmon.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+FASTQ_DIR=$1
+
+SALMON_INDEX="salmon_index"
+
+samples=$(ls $FASTQ_DIR/*_1.fastq.gz | xargs -n1 basename | sed 's/_1.fastq.gz//' | sort | uniq)
+
+for sample in $samples; do
+
+    mkdir -p "$sample"
+
+    fq1="$FASTQ_DIR/${sample}_1.fastq.gz"
+    fq2="$FASTQ_DIR/${sample}_2.fastq.gz"
+
+    salmon quant \
+        -i $SALMON_INDEX \
+        -l A \
+        -1 $fq1 \
+        -2 $fq2 \
+        -o "$sample" \
+        --meta \
+        --minScoreFraction 0.25 \
+        --consensusSlack 0.2 \
+        --maxRecoverReadOcc 1000
+
+done
+
